3feb29e7f60600bf2e2cb66772e7f6ba20b7a889
Philippe Camus <camus@unhcr.org>
2014-02-28 14:15:41 +0100

    commented user creation

--- SQL/LoadPFData.sql
+++ SQL/LoadPFData.sql
@@ -2,10 +2,10 @@ prompt LoadFocalPoint
 prompt ==================
 @@Build/LoadFocalPoint.sql
 
-prompt createPFusers
-prompt ==================
-@@Build/PLSQL/createPFusers.sql
-
 prompt LoadMissingCountryRoles
 prompt ==================
 @@Build/PLSQL/LoadMissingCountryRoles.sql
+
+prompt createPFusers
+prompt ==================
+--@@Build/PLSQL/createPFusers.sql

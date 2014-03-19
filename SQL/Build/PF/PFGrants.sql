87b2e0af664d64ab8f0c610376240dfaba36afff
Philippe Camus <camus@unhcr.org>
2014-03-19 17:13:59 +0100

    Grants for PF roles

---
+++ SQL/Build/PF/PFGrants.sql
@@ -0,0 +1,23 @@
+grant select on PF_FOCAL_POINT to PSR_PF_DATA_ENTRY;
+grant select on PF_PFDEC to PSR_PF_DATA_ENTRY;
+grant select on PF_PFINC to PSR_PF_DATA_ENTRY;
+grant select on PF_PFPOC to PSR_PF_DATA_ENTRY;
+grant select on PF_PPG_COUNTRY to PSR_PF_DATA_ENTRY;
+grant select on PF_RETSTOCK to PSR_PF_DATA_ENTRY;
+grant select on PF_SUMMARY_STATISTICS to PSR_PF_DATA_ENTRY;
+grant select on COUNTRIES to PSR_PF_DATA_ENTRY;
+grant select on CURR_USER_COUNTRIES to PSR_PF_DATA_ENTRY;
+grant select on CURR_USER_ROLES to PSR_PF_DATA_ENTRY;
+grant select on DIMENSION_VALUES to PSR_PF_DATA_ENTRY;
+grant select on DISPLACEMENT_STATUSES to PSR_PF_DATA_ENTRY;
+grant select on LOCATIONS to PSR_PF_DATA_ENTRY;
+grant select on SYSTEM_USERS to PSR_PF_DATA_ENTRY;
+grant select on USER_ROLES to PSR_PF_DATA_ENTRY;
+grant select on ROLES to PSR_PF_DATA_ENTRY;
+grant select on ROLE_COUNTRIES to PSR_PF_DATA_ENTRY;
+
+grant execute on P_PF to PSR_PF_DATA_ENTRY;
+grant execute on P_CONTEXT to PSR_PF_DATA_ENTRY;
+grant execute on P_SYSTEM_USER to PSRAPP;
+
+
